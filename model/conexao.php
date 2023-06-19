273863f77521eac6da53194458706d4b8f1b04db
Enzo <ezbecker@inf.ufsm.br>
2023-06-18 23:39:15 -0300

    hospedagem

--- model/conexao.php
+++ model/conexao.php
@@ -1,8 +1,17 @@
 <?php
-$MYSQL_URL = $_ENV['MYSQL_URL'];
+$DB_HOST = $_ENV['DB_HOST'];
+$DB_USER = $_ENV['DB_USER'];
+$DB_PASSWORD = $_ENV['DB_PASSWORD'];
+$DB_DATABASE = $_ENV['DB_DATABASE'];
+$DB_NAME = $_ENV['DB_NAME'];
+$DB_PORT = $_ENV['DB_PORT'];
 
-try {
-	$conectado = new PDO($MYSQL_URL);
-} catch (PDOException $e) {
-	echo "Erro ao conectar: " . $e->getMessage();
+// Conectar ao banco de dados usando as variáveis ​​definidas acima
+$conectado = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
+
+// Verificar se a conexão foi bem-sucedida
+if ($conectado->connect_error) {
+	die("Erro ao conectar: " . $conectado->connect_error);
 }
+
+echo "Conexão bem-sucedida!";
