5238d30cbe07ac0c2dda15ddb76129553fc4cbf2
Enzo <ezbecker@inf.ufsm.br>
2023-06-18 23:13:14 -0300

    hospedagem

--- model/conexao.php
+++ model/conexao.php
@@ -1,11 +1,11 @@
 <?php
-define('DB_SERVER', 'localhost:3307');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'scaperoom');
+$DB_SERVER = $_ENV['DB_SERVER'];
+$DB_USERNAME = $_ENV['DB_USERNAME'];
+$DB_PASSWORD = $_ENV['DB_PASSWORD'];
+$DB_NAME = $_ENV['DB_NAME'];
 
 try {
-	$conectado = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+	$conectado = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
 } catch (Exception $e) {
 	echo "Erro ao conectar: " . $e->getMessage();
 }
