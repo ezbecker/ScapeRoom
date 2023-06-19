ae7bad3011fe91826f06754183b431e95f2ed3f5
Larissa Pretto <larissapretto009@gmail.com>
2023-06-19 20:00:46 -0300

    ffe

--- view/game.php
+++ view/game.php
@@ -7,8 +7,20 @@ if (isset($_SESSION['pagina']) && isset($_SESSION['idPuzzle'])) {
     $pagina = $_SESSION['pagina'];
     $idPuzzle = $_SESSION['idPuzzle'];
 }
+$email = $_SESSION["email"];
 
-require_once "../model/pegarIdUsuario.php";
+$query = "SELECT * FROM usuario WHERE email = '$email'";
+$stmt = mysqli_prepare($conectado, $query);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+if (mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    $idUsuario = $row["idUsuario"];
+    $nome = $row["nome"];
+} else {
+    echo "Usuário não encontrado.";
+}
+
 
 $query = "SELECT * FROM partida WHERE idUsuario = $idUsuario ORDER BY idPartida DESC LIMIT 1";
 $stmt = mysqli_prepare($conectado, $query);
