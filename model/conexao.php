8e7871ab9242468d9b4ff02ed8e792ceeaed88cb
Enzo <ezbecker@inf.ufsm.br>
2023-06-19 01:26:36 -0300

    hospedagem

--- model/conexao.php
+++ model/conexao.php
@@ -6,10 +6,8 @@ $DB_DATABASE = $_ENV['DB_DATABASE'];
 $DB_NAME = $_ENV['DB_NAME'];
 $DB_PORT = $_ENV['DB_PORT'];
 
-// Conectar ao banco de dados usando as variáveis ​​definidas acima
 $conectado = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
 
-// Verificar se a conexão foi bem-sucedida
 if ($conectado->connect_error) {
 	die("Erro ao conectar: " . $conectado->connect_error);
 }
--- model/incluirUser.php
+++ model/incluirUser.php
@@ -5,20 +5,16 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-// Validar e-mail
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 	die('E-mail inválido');
 }
 
-// Validar senha
 if (strlen($senha) < 8) {
 	die('A senha deve ter pelo menos 8 caracteres');
 }
 
-// Hash de senha
 $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
 
-// Inserir dados com prepared statement
 $stmt = $conectado->prepare("INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $nome, $email, $senhaHash);
 
