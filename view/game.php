4043ac1bcc67928dcdbc02fa88c06470fb2c6fd3
Larissa Pretto <larissapretto009@gmail.com>
2023-06-19 20:07:01 -0300

    dddddd

--- view/game.php
+++ view/game.php
@@ -9,18 +9,6 @@ if (isset($_SESSION['pagina']) && isset($_SESSION['idPuzzle'])) {
 }
 $email = $_SESSION["email"];
 
-$query = "SELECT * FROM usuario WHERE email = '$email'";
-$stmt = mysqli_prepare($conectado, $query);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-if (mysqli_num_rows($result) === 1) {
-    $row = mysqli_fetch_assoc($result);
-    $idUsuario = $row["idUsuario"];
-    $nome = $row["nome"];
-} else {
-    echo "Usuário não encontrado.";
-}
-
 ?>
 
 <head>
