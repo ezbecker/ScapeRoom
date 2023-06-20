41a35a6ca198b66e1c55ec6a40c554cd140e1022
Larissa Pretto <larissapretto009@gmail.com>
2023-06-19 22:16:40 -0300

    agora vaiiiiiiiiii

--- model/salvarTempo.php
+++ model/salvarTempo.php
@@ -1,6 +1,17 @@
 <?php
-@session_start();
-require_once "../model/pegarIdUsuario.php";
+session_start();
+require_once "../model/conexao.php";
+$email = $_SESSION['email'];
+
+$query = "SELECT * FROM usuario WHERE email = '$email'";
+$stmt = mysqli_prepare($conectado, $query);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+if (mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    $idUsuario = $row["idUsuario"];
+    $nome = $row["nome"];
+}
 $tempo = $_POST['tempo'];
 $idPartida = $_POST['idPartida'];
 
