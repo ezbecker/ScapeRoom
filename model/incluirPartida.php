04c23a65bdb837b5c1c4b92f2ac04cc7557dad54
Larissa Pretto <larissapretto009@gmail.com>
2023-06-19 21:11:00 -0300

    efsfd

--- model/incluirPartida.php
+++ model/incluirPartida.php
@@ -1,6 +1,15 @@
 <?php
 //session_start();
-require_once "../model/pegarIdUsuario.php";
+$email = 'larissapretto009w@gmail.com';
+$query = "SELECT * FROM usuario WHERE email = '$email'";
+$stmt = mysqli_prepare($conectado, $query);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+if (mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    $idUsuario = $row["idUsuario"];
+    $nome = $row["nome"];
+}
 
 if (mysqli_num_rows($result) === 1) {
     $tempo = '00:40:00';
