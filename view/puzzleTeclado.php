6d07d6a246e0368bc1c2277fbe8b982b995f7a25
Larissa Pretto <larissapretto009@gmail.com>
2023-06-19 22:06:07 -0300

    tecaldo

--- view/puzzleTeclado.php
+++ view/puzzleTeclado.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <?php
-require_once "../controller/userAutenticado.php";
-require_once "../model/pegarIdUsuario.php";
+session_start();
+
 require_once "../model/conexao.php";
+$query = "SELECT * FROM usuario WHERE email = '$email'";
+$stmt = mysqli_prepare($conectado, $query);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+if (mysqli_num_rows($result) === 1) {
+  $row = mysqli_fetch_assoc($result);
+  $idUsuario = $row["idUsuario"];
+  $nome = $row["nome"];
+}
 
 $idPuzzle = $_GET["idPuzzle"];
 $query = "SELECT * FROM partida WHERE idUsuario = $idUsuario ORDER BY idPartida DESC LIMIT 1";
