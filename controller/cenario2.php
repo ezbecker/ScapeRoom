3660c9c22fd392727923a286148ce94057d5112c
Enzo <ezbecker@inf.ufsm.br>
2023-06-28 14:17:16 -0300

    teste

--- controller/cenario2.php
+++ controller/cenario2.php
@@ -4,7 +4,7 @@
 
 <script>
     var puzzle = [
-        "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"
+        "3"//, "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"
     ];
     var index = Math.floor(Math.random() * puzzle.length);
     var paginaSelecionada = 24;
